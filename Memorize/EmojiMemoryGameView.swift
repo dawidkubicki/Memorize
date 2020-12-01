f6b59765ef69dee76a4c1801360d86c71f70f9ce
Dawid Kubicki <dawid@pluscode.io>
2020-12-01 21:43:49 +0100

    minor changes

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,14 +14,14 @@ struct EmojiMemoryGameView: View {
     var body: some View {
         HStack{
             ForEach(viewModel.cards) { card in
-                CardView(card: card).onTapGesture(perform: {
+                CardView(card: card).onTapGesture{
                     viewModel.choose(card: card)
-                })
+                }
             }
         }
         .padding()
         .foregroundColor(.orange)
-        .font(.largeTitle)
+        
     }
 }
 
@@ -36,16 +36,26 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
-        ZStack{
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0)
-                    .stroke(lineWidth: 3.0)
-                Text(card.content)
-                    .font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+        GeometryReader{ geometry in
+            ZStack{
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .stroke(lineWidth: edgeLineWith)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
+                }
             }
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * fontScaleFactor))
+            
         }
     }
+    
+    //MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWith: CGFloat = 3
+    let fontScaleFactor: CGFloat = 0.75
+    
 }
