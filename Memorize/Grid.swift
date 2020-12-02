98dbf8937e9bd9ac847fd7f494aec1966c569e9e
Dawid Kubicki <dawid@pluscode.io>
2020-12-02 22:49:16 +0100

    functions in geometry

--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Grid<Item, ItemView>: View {
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     
     var items: [Item]
     var viewForItem: (Item) -> ItemView
@@ -18,6 +18,33 @@ struct Grid<Item, ItemView>: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            self.body(for: GridLayout(itemCount: self.items.count, in: geometry.size))
+        }
+    }
+    
+    func body(for layout: GridLayout) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: layout)
+        }
+    }
+    
+    func body(for item: Item, in layout: GridLayout) -> some View {
+        
+        let index = self.index(of: item)
+        
+        return viewForItem(item)
+            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+            .position(layout.location(ofItemAt: index))
+    }
+    
+    func index(of item: Item) -> Int {
+        
+        for index in 0..<items.count {
+            if items[index].id == item.id {
+                return index
+            }
+        }
+        return 0
     }
 }
