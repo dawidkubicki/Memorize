ab677028136364536413344eb3baf34a10fcdb3d
Dawid Kubicki <dawid@pluscode.io>
2022-02-13 13:53:18 +0100

    minor change in UI

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,26 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var emojis: [String] = ["⛷", "🚡", "📚", "🧅", "✊", "🦌", "⏰", "⚖️", "👻", "👽", "😸", "🎃", "🐇", "🌴", "🥎", "🛹", "⛳️", "🏀", "🍴"]
-    @State var emojiCount = 4
+    var emojis: [String] = ["⛷", "🚡", "📚", "🧅", "✊", "🦌", "⏰", "⚖️", "👻", "👽", "😸", "🎃", "🐇", "🌴", "🥎", "🛹", "⛳️", "🏀", "🍴", "🏋️‍♀️", "🥋", "🥊"]
+    @State var emojiCount = 17
     
     var body: some View {
         VStack{
-            HStack {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum:  75))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
+                .foregroundColor(.red)
+                .padding(.horizontal)
             }
-            .foregroundColor(.red)
-            .padding(.horizontal)
-            
+            Spacer()
             HStack {
                 remove
                 Spacer()
                 add
             }
             .font(.largeTitle)
-            .padding(.vertical)
             .padding(.horizontal)
         }
     }
@@ -64,7 +65,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 25)
             if isFaceUp {
                 shape.fill(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content)
                     .font(.largeTitle)
                     .padding()
@@ -108,6 +109,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.light)
+.previewInterfaceOrientation(.portrait)
         ContentView()
             .preferredColorScheme(.dark)
     }
